454962cbe38ef4d690cd78ae319f7998287f0577
clickwir <clickwir631@gmail.com>
2015-03-17 03:59:49 -0400

    Update ubuntu-settings.sh
    SSH fixes were trying to find config files and script had errors. Set config file and removed find function.
    Cleaned up SSH security section to remove runcmd option.

--- ubuntu-settings.sh
+++ ubuntu-settings.sh
@@ -98,171 +98,89 @@ echo "\n... Improving SSH security, this will take a while."
 echo "... This is the last step before running a dist-upgrade."
 echo "... dist-upgrade is the last step.\n"
 
-DRY_RUN=0;
-while getopts 'hd' option; do
-        case "$option" in
-                'h')
-                        usage;
-                        exit 1;
-                        ;;
-                'd')
-                        DRY_RUN=1;
-                        ;;
-                ?)
-                        usage;
-                        exit 1;
-                        ;;
-        esac;
-done;
-# }}}
-
-runcmd() { # {{{
-        echo "+++ $@";
-        if [ "$DRY_RUN" -eq 0 ]; then
-                cmd="$1";
-                shift;
-                $cmd "$@";
-        fi;
-} # }}}
-
 # Try to find config files {{{
-SSHD_CONFIG_POSSIBILITIES=(
-        '/etc/ssh/sshd_config'
-        '/etc/sshd_config'
-);
-SSH_CONFIG_POSSIBILITIES=(
-        '/etc/ssh/ssh_config'
-        '/etc/ssh_config'
-);
+SSHD_CONFIG_POSSIBILITIES="/etc/ssh/sshd_config"
+SSH_CONFIG_POSSIBILITIES="/etc/ssh/ssh_config"
+SSHD_CONFIG="/etc/ssh/sshd_config";
+SSH_CONFIG="/etc/ssh/ssh_config";
 
-SSHD_CONFIG='';
-SSH_CONFIG='';
-
-for file in ${SSHD_CONFIG_POSSIBILITIES[*]}; do
-        if [ -f "$file" ]; then
-                echo "--- Found SSHD_CONFIG at ${file}";
-                SSHD_CONFIG="${file}";
-                break;
-        fi;
-done
-
-for file in ${SSH_CONFIG_POSSIBILITIES[*]}; do
-        if [ -f "$file" ]; then
-                echo "--- Found SSH_CONFIG at ${file}";
-                SSH_CONFIG="${file}";
-                break;
-        fi;
-done;
-# }}}
-
-# Fix sshd config if we found it {{{
-if [ "$SSHD_CONFIG" != '' ]; then
-        echo "\n... Fix sshd config if we found it"
+# Fix sshd config
+        echo "\n... Fix sshd config"
         SSHD_CONFIG_DIR="$(dirname "$SSHD_CONFIG")";
         lines_inserted=0;
 
         # Fix key exchange algorithm settings if needed
         grep '^\s*KexAlgorithms\s\+' "$SSHD_CONFIG" &>/dev/null;
-        if [ "$?" -eq 0 ]; then
-               echo "\n... Fix key exchange algorithm settings if needed"
-                runcmd sed -i 's/^\(\s*\)KexAlgorithms\s\+.*$/\1KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256/' "$SSHD_CONFIG";
-                sleep 1
-        else
-                lines_inserted=$((${lines_inserted} + 1));
                 echo "\n... Fix key exchange algorithm settings if needed"
-                runcmd sed -i "${lines_inserted}i\\KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256" "$SSHD_CONFIG";
+                sed -i 's/^\(\s*\)KexAlgorithms\s\+.*$/\1KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256/' "$SSHD_CONFIG";
                 sleep 1
-        fi;
 
         # If the moduli file exists, get rid of any primes less than 2000 bits
         MODULI="${SSHD_CONFIG_DIR}/moduli";
         if [ -f "$MODULI" ]; then
                 # Ugly hack for portable in-place awk
                 echo "\n... If the moduli file exists, get rid of any primes less than 2000 bits"
-                runcmd awk '$5 > 2000' "$MODULI" > >(cat <(sleep 1) - > "$MODULI");
+                awk '$5 > 2000' "$MODULI" > >(cat <(sleep 1) - > "$MODULI");
                 sleep 1
         else
-                runcmd touch "$MODULI";
+                touch "$MODULI";
                 sleep 1
         fi;
 
         # If there's nothing left in the moduli file (or it didn't exist at all), we should populate it
         if [ "$(stat --printf=%s "$MODULI")" -lt 10 ]; then
                 echo "\n... If there's nothing left in the moduli file (or it didn't exist at all), we should populate it"
-                runcmd rm "$MODULI";
-                runcmd ssh-keygen -q -G "$SSHD_CONFIG_DIR"/moduli.tmp -b 4096
-                runcmd ssh-keygen -T "$MODULI" -f "$SSHD_CONFIG_DIR/moduli.tmp"
-                runcmd rm -f "$SSHD_CONFIG_DIR/moduli.tmp"
+                rm "$MODULI";
+                ssh-keygen -q -G "$SSHD_CONFIG_DIR"/moduli.tmp -b 4096
+                ssh-keygen -T "$MODULI" -f "$SSHD_CONFIG_DIR/moduli.tmp"
+                rm -f "$SSHD_CONFIG_DIR/moduli.tmp"
                 sleep 1
         fi;
 
         # Force v2 protocol
         grep '^\s*Protocol\s\+' "$SSHD_CONFIG" &>/dev/null;
-        if [ "$?" -eq 0 ]; then
                 echo "\n... Force v2 protocol"
-                runcmd sed -i 's/^\(\s*\)Protocol\s\+.*$/\1Protocol 2/' "$SSHD_CONFIG";
+                sed -i 's/^\(\s*\)Protocol\s\+.*$/\1Protocol 2/' "$SSHD_CONFIG";
                 sleep 1
-        else
-                lines_inserted=$((${lines_inserted} + 1));
-                echo "\n... Force v2 protocol"
-                runcmd sed -i "${lines_inserted}iProtocol 2" "$SSHD_CONFIG";
-                sleep 1
-        fi;
 
         # Get rid of DSA and ECDSA keys; create RSA and Ed25519 if they don't exist
         echo "\n... Get rid of DSA and ECDSA keys; create RSA and Ed25519 if they don't exist"
-        runcmd sed -i '/^\s*HostKey/d' "$SSHD_CONFIG";
+        sed -i '/^\s*HostKey/d' "$SSHD_CONFIG";
         sleep 1
         lines_inserted=$((${lines_inserted} + 1));
-        runcmd sed -i "${lines_inserted}iHostKey ${SSHD_CONFIG_DIR}/ssh_host_ed25519_key" "$SSHD_CONFIG";
+        sed -i "${lines_inserted}iHostKey ${SSHD_CONFIG_DIR}/ssh_host_ed25519_key" "$SSHD_CONFIG";
         sleep 1
         lines_inserted=$((${lines_inserted} + 1));
-        runcmd sed -i "${lines_inserted}iHostKey ${SSHD_CONFIG_DIR}/ssh_host_rsa_key" "$SSHD_CONFIG";
+        sed -i "${lines_inserted}iHostKey ${SSHD_CONFIG_DIR}/ssh_host_rsa_key" "$SSHD_CONFIG";
         sleep 1
-        runcmd rm -f "${SSHD_CONFIG_DIR}/ssh_host_key{,.pub}";
+        rm -f "${SSHD_CONFIG_DIR}/ssh_host_key{,.pub}";
         sleep 1
-        runcmd rm -f "${SSHD_CONFIG_DIR}/ssh_host_dsa_key{,.pub}";
+        rm -f "${SSHD_CONFIG_DIR}/ssh_host_dsa_key{,.pub}";
         sleep 1
-        runcmd rm -f "${SSHD_CONFIG_DIR}/ssh_host_ecdsa_key{,.pub}";
+        rm -f "${SSHD_CONFIG_DIR}/ssh_host_ecdsa_key{,.pub}";
         sleep 1
-        runcmd rm -f "${SSHD_CONFIG_DIR}/ssh_host_rsa_key{,.pub}";
+        rm -f "${SSHD_CONFIG_DIR}/ssh_host_rsa_key{,.pub}";
         sleep 1
         if [ ! -f "${SSHD_CONFIG_DIR}/ssh_host_ed25519_key" ] || [ ! -f "${SSHD_CONFIG_DIR}/ssh_host_ed25519_key.pub" ]; then
-                runcmd ssh-keygen -t ed25519 -f "${SSHD_CONFIG_DIR}/ssh_host_ed25519_key" < /dev/null;
+                ssh-keygen -t ed25519 -f "${SSHD_CONFIG_DIR}/ssh_host_ed25519_key" < /dev/null;
                 sleep 1
         fi;
         if [ ! -f "${SSHD_CONFIG_DIR}/ssh_host_rsa_key" ] || [ ! -f "${SSHD_CONFIG_DIR}/ssh_host_rsa_key.pub" ]; then
-                runcmd ssh-keygen -t rsa -b 4096 -f "${SSHD_CONFIG_DIR}/ssh_host_rsa_key" < /dev/null;
+                ssh-keygen -t rsa -b 4096 -f "${SSHD_CONFIG_DIR}/ssh_host_rsa_key" < /dev/null;
                 sleep 1
         fi;
 
         # Limit symmetric ciphers to good modern ones
         grep '^\s*Ciphers\s\+' "$SSHD_CONFIG" &>/dev/null;
-        if [ "$?" -eq 0 ]; then
                 echo "\n... Limit symmetric ciphers to good modern ones"
-                runcmd sed -i 's/^\(\s*\)Ciphers\s\+.*$/\1Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr/' "$SSHD_CONFIG";
+                sed -i 's/^\(\s*\)Ciphers\s\+.*$/\1Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr/' "$SSHD_CONFIG";
                 sleep 1
-        else
-                lines_inserted=$((${lines_inserted} + 1));
-                echo "\n... Limit symmetric ciphers to good modern ones"
-                runcmd sed -i "${lines_inserted}iCiphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr" "$SSHD_CONFIG";
-                sleep 1
-        fi;
 
         # Limit MAC algos to good modern ones with long keys, ETM only
         grep '^\s*MACs\s\+' "$SSHD_CONFIG" &>/dev/null;
-        if [ "$?" -eq 0 ]; then
                 echo "\n... Limit MAC algos to good modern ones with long keys, ETM only"
-                runcmd sed -i 's/^\(\s*\)MACs\s\+.*$/\1MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-ripemd160-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,hmac-ripemd160,umac-128@openssh.com/' "$SSHD_CONFIG";
+                sed -i 's/^\(\s*\)MACs\s\+.*$/\1MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-ripemd160-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,hmac-ripemd160,umac-128@openssh.com/' "$SSHD_CONFIG";
                 sleep 1
-        else
-                lines_inserted=$((${lines_inserted} + 1));
-                echo "\n... Limit MAC algos to good modern ones with long keys, ETM only"
-                runcmd sed -i "${lines_inserted}iMACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-ripemd160-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,hmac-ripemd160,umac-128@openssh.com" "$SSHD_CONFIG";
-                sleep 1
-        fi;
-fi;
-# }}}
 
 # Some settings
 echo "\n... Disable logout, restart and shutdown prompts."
